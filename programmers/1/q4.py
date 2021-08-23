0977a945d01928788622f1be53f8fc79361c2a19
Dongmin <ehdals338@gmail.com>
2021-08-23 10:41:00 +0900

    Initial commit

--- programmers/1/q4.py
+++ programmers/1/q4.py
@@ -31,6 +31,7 @@ def solution(phone_number):
     return "*" * (len(phone_number)-4) + phone_number[-4:]
 
 vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+for example
 
 phone_number = "01033334444"
 print ("*" * (len(phone_number)-4) + phone_number[-4:])
